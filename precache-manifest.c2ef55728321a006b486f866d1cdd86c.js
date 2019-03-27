efeec6f4d5f2c78b28b5f4ab98d3c041f2ccef28
Daniel Skowron <skofikit@gmail.com>
2019-03-27 20:00:39 +0100

    Updates

---
+++ precache-manifest.c2ef55728321a006b486f866d1cdd86c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e547a578794634635212f480b58e091d",
+    "url": "/Daily-calories-counter/static/media/dark.e547a578.png"
+  },
+  {
+    "revision": "1f17f7da75b9062f648c",
+    "url": "/Daily-calories-counter/static/js/runtime~main.aea54a95.js"
+  },
+  {
+    "revision": "2f57e72c9ad1bde328ff",
+    "url": "/Daily-calories-counter/static/js/main.adba2336.chunk.js"
+  },
+  {
+    "revision": "c411c6295b691d07baae",
+    "url": "/Daily-calories-counter/static/js/2.500d5162.chunk.js"
+  },
+  {
+    "revision": "2f57e72c9ad1bde328ff",
+    "url": "/Daily-calories-counter/static/css/main.65c95143.chunk.css"
+  },
+  {
+    "revision": "c7b4e5a11db1217dc748602869409483",
+    "url": "/Daily-calories-counter/index.html"
+  }
+];
